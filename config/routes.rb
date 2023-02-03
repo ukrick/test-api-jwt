7474d65150eea11e9c239a09c2492d3a4b48bbac
Andrew Kirillov <andrew.a.kirillov@yandex.com>
2023-02-03 19:33:43 +0200

    create Devise controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users,
+             defaults: { format: :json },
+             path: '',
+             path_names: {
+               sign_in: 'login',
+               sign_out: 'logout',
+               registration: 'signup'
+             },
+             controllers: {
+               sessions: 'users/sessions',
+               registrations: 'users/registrations'
+             }
 
   namespace :system do
     get 'dashboard', to: 'dashboard#index'

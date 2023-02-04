faedb3bd1387419bfc9d7faf9e6e82fb46900e92
Andrew Kirillov <andrew.a.kirillov@yandex.com>
2023-02-04 11:55:20 +0200

    respond to requests in JSON format

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::API
+  include ActionController::MimeResponds
+  include ActionController::RequestForgeryProtection
+
+  protect_from_forgery with: :null_session, if: proc { |c| c.request.format =~ %r{application/json} }
+
+  respond_to :json
 end
